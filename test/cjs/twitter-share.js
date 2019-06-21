30434b20e086299765e3b2a2996867bf3bc6507e
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-06-21 21:04:01 +0200

    added twitter-share

---
+++ test/cjs/twitter-share.js
@@ -0,0 +1,57 @@
+'use strict';
+// original implementation here
+// https://svelte.dev/repl/98aa20d4cb3d40dabfef7d8dae183b85?version=3.5.2
+const TwitterShare = {
+  extends: 'a',
+  style: (TS) => `
+    ${TS} {
+      background: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 400 400'%3E%3Cpath fill='rgb(29, 161, 242)' class='cls-2' d='M153.62,301.59c94.34,0,145.94-78.16,145.94-145.94,0-2.22,0-4.43-.15-6.63A104.36,104.36,0,0,0,325,122.47a102.38,102.38,0,0,1-29.46,8.07,51.47,51.47,0,0,0,22.55-28.37,102.79,102.79,0,0,1-32.57,12.45,51.34,51.34,0,0,0-87.41,46.78A145.62,145.62,0,0,1,92.4,107.81a51.33,51.33,0,0,0,15.88,68.47A50.91,50.91,0,0,1,85,169.86c0,.21,0,.43,0,.65a51.31,51.31,0,0,0,41.15,50.28,51.21,51.21,0,0,1-23.16.88,51.35,51.35,0,0,0,47.92,35.62,102.92,102.92,0,0,1-63.7,22A104.41,104.41,0,0,1,75,278.55a145.21,145.21,0,0,0,78.62,23'/%3E%3C/svg%3E") 0 50% no-repeat;
+      background-size: 1.5em;
+      color: rgb(29, 161, 242);
+      font-weight: bold;
+      padding: 0.5em 0.3em 0.5em 1.5em;
+      text-decoration: none;
+    }
+    ${TS}:hover {
+      border-bottom: 2px solid rgb(29, 161, 242);
+    }
+  `,
+  observedAttributes: ['text', 'url', 'hashtags', 'via', 'related'],
+  onattributechanged(event) {
+    this.setAttribute('href', getHref(this));
+    this.setAttribute('noreferrer', '');
+    this.textContent = 'Tweet this';
+  },
+  onclick(e) {
+    e.preventDefault();
+    const w = 600;
+    const h = 400;
+    const x = (screen.width - w) / 2;
+    const y = (screen.height - h) / 2;
+    const features = `width=${w},height=${h},left=${x},top=${y}`;
+    window.open(this.href, '_blank', features);
+  },
+  onconnected() {
+    this.addEventListener('click', this);
+    // TODO: requires a special `ssr: {onconnected(){}}` property
+    if (!('href' in this))
+      this.attributes = this.attributes.filter(
+        attr => !TwitterShare.observedAttributes.includes(attr.name)
+      );
+  }
+};
+
+Object.defineProperty(exports, '__esModule', {value: true}).default = TwitterShare;
+
+function getHref(self) {
+  return 'https://twitter.com/intent/tweet?' +
+          TwitterShare.observedAttributes.reduce(
+            (qs, curr) => {
+              const val = self.getAttribute(curr);
+              if (val)
+                qs.push(curr + '=' + encodeURIComponent(val));
+              return qs;
+            },
+            []
+          ).join('&');
+}
---
+++ test/twitter-share.js
@@ -0,0 +1,62 @@
+const {document, define, render, html} = require('../cjs');
+const {default: TwitterShare} = require('./cjs/twitter-share');
+
+// generic document head setup
+render(document.head, html`
+  <title>🔥 heresy SSR 🔥 TwitterShare Example</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script>
+  if(this.customElements)
+    try{customElements.define('built-in',document.createElement('p').constructor,{'extends':'p'})}
+    catch(s){document.head.appendChild(document.createElement('script')).src='https://unpkg.com/@ungap/custom-elements-builtin'}
+  else
+    document.write('<script src="https://unpkg.com/document-register-element"><\x2fscript>');
+  </script>
+  <!--👻 default basic styles can be sent right away through the page -->
+  <style>
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI",
+                  Roboto, Oxygen-Sans, Ubuntu, Cantarell,
+                  "Helvetica Neue", sans-serif;
+  }
+  </style>
+  <!--👻
+    heresy can land in this page at any time,
+    it's the component definition that matters
+  -->
+  <script async type="module">
+  import {define} from 'https://unpkg.com/heresy?module';
+  import TwitterShare from '/esm/twitter-share.js';
+  // that's it, rehydration out of the box
+  define('TwitterShare', TwitterShare);
+  </script>`
+);
+
+// define the component like a Custom Element
+define('TwitterShare', TwitterShare);
+
+// and use it whenever you want
+render(document.body, html`
+  <TwitterShare
+    text="A Twitter share button with progressive enhancement"
+    url="https://github.com/WebReflection/heresy#readme"
+    via="webreflection"
+  />`
+);
+
+// spin a server to test this
+require('http').createServer((req, res) => {
+  switch (true) {
+    case /^\/esm\/.+$/.test(req.url):
+      res.writeHead(200, {"content-type": "application/javascript"});
+      res.end(require('fs').readFileSync(`./test${req.url}`));
+      break;
+    default:
+      res.writeHead(200, {"content-type": "text/html;charset=utf-8"});
+      res.end(document.toString());
+      break;
+  }
+}).listen(8080);
+
+console.log('http://localhost:8080/');
